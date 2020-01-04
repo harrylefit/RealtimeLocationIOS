ce6e3a33144f6932fc8f74944a56b4a4fc855650
Harry Le <harryle.fit@gmail.com>
2020-01-04 14:23:55 +0700

    init project

---
+++ RealtimLocationControl/RealtimLocationControl/RealtimLocationControl.h
@@ -0,0 +1,19 @@
+//
+//  RealtimLocationControl.h
+//  RealtimLocationControl
+//
+//  Created by Harry Le on 1/4/20.
+//  Copyright © 2020 Harry Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RealtimLocationControl.
+FOUNDATION_EXPORT double RealtimLocationControlVersionNumber;
+
+//! Project version string for RealtimLocationControl.
+FOUNDATION_EXPORT const unsigned char RealtimLocationControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealtimLocationControl/PublicHeader.h>
+
+
---
+++ RealtimeLocationShowcase/RealtimeLocationShowcase/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RealtimeLocationShowcase
+//
+//  Created by Harry Le on 1/4/20.
+//  Copyright © 2020 Harry Le. All rights reserved.
+//
+
+import UIKit
+import RealtimLocationControl
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
